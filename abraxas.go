8b647dd53a218e661468ef6278e0e594b83ac66a
aiubatti <aiubatti@edgelab.ch>
2021-11-04 10:58:45 +0100

    code: extract logger

--- abraxas.go
+++ abraxas.go
@@ -1,7 +1,9 @@
 package abraxas
 
+import "github.com/antonioiubatti93/abraxas/logger"
+
 const answer = "I'm Abraxas, at your service!"
 
-func Invoke(logger Logger) {
+func Invoke(logger logger.Logger) {
 	logger.Print(answer)
 }
